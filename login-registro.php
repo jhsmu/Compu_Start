087d98748ba9b5966d0880bb493c578ee4728dec
Miguel Zapata <maxmo968@gmail.com>
2023-02-22 16:32:30 -0500

    Validaciones

--- login-registro.php
+++ login-registro.php
@@ -4,13 +4,42 @@
 	require_once 'conexion.php';
 
     if(isset($_POST["crear"])){
-        $nombre=$_POST["nombre"];
-        $usuario=$_POST["usuario"];
-        $direccion=$_POST["direccion"];
-        $telefono=$_POST["telefono"];
-        $email=$_POST["email"];
-        $contrasena=$_POST["clave"];
+        if(preg_match('/^[a-zñÑ A-Z]+$/',$_POST["nombre"])== 1){
+            $nombre = $_POST["nombre"];
+        }else{echo '<script>confirm("Porfavor ingrese un nombre valido")</script>';}
 
+        if(preg_match('/^[0-9A-Za-z]+$/',$_POST["usuario"])== 1){        
+            $usuario = $_POST["usuario"];
+        }else{echo '<script>confirm("Porfavor ingrese un usuario valido")</script>';}
+
+        $direccion= htmlentities($_POST["direccion"]);
+
+        if(preg_match('/^[0-9]+$/',$_POST["telefono"])== 1){
+            $telefono = $_POST["telefono"];
+        }else{echo '<script>confirm("Porfavor ingrese un número de telefono valido")</script>';}
+
+        if(preg_match("/(^([A-Za-z.]|@|[0-9])+$)/",$_POST["email"])== 1){
+            $email = $_POST["email"];
+        }else{echo '<script>confirm("Porfavor ingrese un email valido")</script>';}
+
+        if(strlen($_POST["clave"]) > 8){
+            if(strlen($_POST["clave"]) < 16){
+                if (preg_match('`[a-z]`',$_POST["clave"])){
+                    if (preg_match('`[A-Z]`',$_POST["clave"])){
+                        if (preg_match('`[0-9]`',$_POST["clave"])){
+                            $contrasena=$_POST["clave"];
+                            }else
+                            {echo '<script>confirm("la clave debe tener al menos un número")</script>';}
+                        } else
+                        {echo '<script>confirm("La contraseña debe tener al menos una mayuscula")</script>';}              
+                    }else
+                        {echo '<script>confirm("La contraseña debe tener una minuscula")</script>';} 
+                }else
+                    {echo '<script>confirm("La contraseña no puede tener mas de 16 caracteres")</script>';}  
+            }else 
+            {echo '<script>alert("La contraseña debe tener minimo 8 digitos")</script>'; }                           
+    }
+    if(isset($contrasena)){
         $agregar=$DB_con->prepare('INSERT INTO cliente(usuario, nombre, email, direccion, telefono, contrasenia) VALUES(:usuario, :nombre, :email, :direccion, :telefono, :contrasenia)');
         $agregar->bindParam(':usuario', $usuario);
         $agregar->bindParam(':nombre', $nombre);
@@ -18,7 +47,7 @@
         $agregar->bindParam(':direccion', $direccion);
         $agregar->bindParam(':telefono', $telefono);
         $agregar->bindParam(':contrasenia', $contrasena);
-
+            
         if ($agregar->execute()) {
             header("location:si.php");
         }
@@ -26,9 +55,10 @@
             header("location:no.php");
         }
     }
-    elseif(isset($_POST["inicio"])){
-        $usuario_i=$_POST["usuario_inicio"];
-        $contrasena=$_POST["clave_inicio"];
+
+    if(isset($_POST["inicio"])){
+        $usuario_i=htmlentities($_POST["usuario_inicio"]);
+        $contrasena=htmlentities($_POST["clave_inicio"]);
 
         $consultar=$DB_con->prepare('SELECT * FROM cliente WHERE usuario=:usuario');
         $consultar->bindParam(':usuario', $usuario_i);
@@ -80,12 +110,12 @@
                 <input type="number" name="telefono" placeholder="Numero Telefonico" required>
                 <input type="email" name="email" placeholder="Correo" required>
                 <input type="password" name="clave" placeholder="Clave" required>
-                <button name="crear">Crear</button>
+                <button name="crear" >Crear</button>
             </form>
         </div>
                 <!-- iniciar sesion -->
      <div class="sign-in">
-         <form action="" method="post">
+         <form action="" method="post" >
           <h1>Iniciar Sesion</h1>
             <div class="div social-container">
                             
