8aaa1b4d8c5a9449a3cbd5a0c4045efaa47429ab
Freymer Sepúlveda <117765402+FrayRyderX@users.noreply.github.com>
2023-03-03 20:21:04 -0500

    Estable

--- index.php
+++ index.php
@@ -11,33 +11,36 @@ if (isset($_POST["sesion"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
+    <!-- css bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- iconos en fontawesome -->
+    <script src="https://kit.fontawesome.com/4b93f520b2.js" crossorigin="anonymous"></script>
+    <!-- css foote y el header -->
     <link rel="stylesheet" href="./css/footer-header.css">
-    <link rel="icon" type="image/png" sizes="16x16" href="./img/Logo/favicon-16x16.png">
-    <title>Compu Start</title>
+    <!-- css cuerpo -->
+    <link rel="stylesheet" href="./css/style_cuerpo.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
 </head>
 
 <body>
+    <!-- encabezado -->
     <header>
-        <?php
-        include(("./componentes/header.php"))
-        ?>
+    <?php include("./componentes/headerindex.php"); ?>
     </header>
-
-    <div class="container">
+    
+<!-- cuerpo -->
+<div class="container ">
         <div class="row mt-4 mb-4">
             <!-- card 1 -->
             <div class="col-md-4">
                 <div class="card">
                     <figure>
-                        <img src="./img/inicio/monitor gamers asus.jpg" height="200px" class="card-img-top" alt="...">
+                    <img src="./img/inicio/monitor gamers asus.jpg" height="200px" class="card-img-top" alt="...">
                     </figure>
                     <div class="card-body">
-                        <h5 class="card-title">Monitor Gamers Asus|</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <h5 class="card-title">Monitor Gamer Asus ROG Swift Pro PG248QP</h5>
+                        <p class="card-text text-justify" >El Asus ROG Swift OLED PG248QP es un monitor totalmente localizado para el público ‘gamer’. Cuenta con un panel TN de 24,1 pulgadas, una resolución de 1080p.</p>
                         <a href="#" class="btn btn-primary">Ver mas</a>
                     </div>
                 </div>
@@ -46,13 +49,11 @@ if (isset($_POST["sesion"])) {
             <div class="col-md-4">
                 <div class="card">
                     <figure>
-                        <img src="./img/inicio/combo teclado + mouse gamer.jpg" height="200px" class="card-img-top"
-                            alt="...">
+                    <img src="./img/inicio/combo teclado + mouse gamer.jpg" height="200px" class="card-img-top" alt="...">
                     </figure>
                     <div class="card-body">
                         <h5 class="card-title">Combo Teclado + Mouse Gamers</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <p class="text-justify">Combo Teclado+Mouse KY-MK07+KY-M986 JANUS Gamer mecanico Cuatro veces más rápido que los teclados estándar Cada tecla está optimizada para mejorar la experiencia táctil y ofrecer una respuesta superrápida. </p>
                         <a href="#" class="btn btn-primary">Ver mas</a>
                     </div>
                 </div>
@@ -61,13 +62,11 @@ if (isset($_POST["sesion"])) {
             <div class="col-md-4">
                 <div class="card">
                     <figure>
-                        <img src="./img/inicio/memoria ram spectrix D60G.png" height="200px" class="card-img-top"
-                            alt="...">
+                    <img src="./img/inicio/memoria ram spectrix D60G.png" height="200px" class="card-img-top" alt="...">
                     </figure>
                     <div class="card-body">
                         <h5 class="card-title">Memoria Ram Spectrix</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <p class="card-text text-md-justify">ADATA MEMORIA DDR4 XPG SPECTRIX D60, Es RGB de 8GB a 3000Mhz, este módulo D60 está fabricado con chips de alta calidad y un disipador de calor metálico que proporciona una integridad, confiabilidad y estabilidad de la señal excelentes</p>
                         <a href="#" class="btn btn-primary">Ver mas</a>
                     </div>
                 </div>
@@ -76,40 +75,33 @@ if (isset($_POST["sesion"])) {
             <div id="carouselExampleAutoplaying" class="carousel slide mt-3 mb-3" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/4893391_15899281063797455_origin.jpg"
-                            height="250px" class="d-block w-100" alt="...">
+                        <img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/4893391_15899281063797455_origin.jpg" height="250px" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="https://hardzone.es/app/uploads-hardzone.es/2022/02/Intel-vs-AMD-2022.jpg"
-                            height="250px" class="d-block w-100" alt="...">
+                        <img src="https://hardzone.es/app/uploads-hardzone.es/2022/02/Intel-vs-AMD-2022.jpg" height="250px" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Intel_Xeon_Roadmap_Ice_Lake_Sapphire_Rapids_Granite_Rapids_5_2060x1159.png"
-                            height="250px" class="d-block w-100" alt="...">
+                        <img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Intel_Xeon_Roadmap_Ice_Lake_Sapphire_Rapids_Granite_Rapids_5_2060x1159.png" height="250px" class="d-block w-100" alt="...">
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
             </div>
             <!-- card 4 -->
             <div class="col-md-4">
                 <div class="card">
                     <figure>
-                        <img src="./img/inicio/Tarjeta-Grafica-ASUS-TUF-RTX-3070-Ti-OC16572985441.png" height="200px"
-                            class="card-img-top" alt="...">
+                    <img src="./img/inicio/Tarjeta-Grafica-ASUS-TUF-RTX-3070-Ti-OC16572985441.png" height="200px" class="card-img-top" alt="...">
                     </figure>
                     <div class="card-body">
                         <h5 class="card-title">Tarjeta Grafica Asus Rtx 3070</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <p class="card-text">Los componentes básicos de las GPU más rápidas y eficientes del mundo, el nuevo Ampere SM ofrece el doble de rendimiento de FP32 y una eficiencia energética mejorada.</p>
                         <a href="#" class="btn btn-primary">Ver mas</a>
                     </div>
                 </div>
@@ -118,12 +110,11 @@ if (isset($_POST["sesion"])) {
             <div class="col-md-4">
                 <div class="card">
                     <figure>
-                        <img src="./img/inicio/computador de mesa.jpg" height="200px" class="card-img-top" alt="...">
+                    <img src="./img/inicio/ryzen5 5600X.jpg" height="200px" class="card-img-top" alt="...">
                     </figure>
                     <div class="card-body">
-                        <h5 class="card-title">Computador De Mesa</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <h5 class="card-title">Procesador ryazen5 5600</h5>
+                        <p class="card-text">El AMD Ryzen 5 5600X es un procesador de la serie 5000 que dispone de 6 núcleos y 12 subprocesos para que disfrutes de un rendimiento sensacional en un amplio catálogo de juegos y aplicaciones.</p>
                         <a href="#" class="btn btn-primary">Ver mas</a>
                     </div>
                 </div>
@@ -132,32 +123,114 @@ if (isset($_POST["sesion"])) {
             <div class="col-md-4">
                 <div class="card">
                     <figure>
-                        <img src="./img/inicio/Lenovo-IdeaPad-3-14IIL05.jpg" height="200px" class="card-img-top"
-                            alt="...">
+                    <img src="./img/inicio/msi z490 A pro.jpg" height="200px" class="card-img-top" alt="...">
                     </figure>
                     <div class="card-body">
-                        <h5 class="card-title">Lenovo IdeaPad</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <h5 class="card-title">Placa madre msi Z490</h5>
+                        <p class="card-text">La Serie PRO ayuda a los usuarios a trabajar de manera más inteligente al ofrecer una experiencia eficiente y productiva. Con una funcionalidad estable y la construcción de alta calidad.</p>
                         <a href="#" class="btn btn-primary">Ver mas</a>
                     </div>
                 </div>
             </div>
         </div>
+</div>
+
+<div class="container">
+              <!-- barra con animacion -->
+            <div class="color">
+            <div class="loader">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            </div>
+    <div class="row mt-4 mb-4">
+                    <!-- card 1.1 -->
+                    <div class="col-md-4">
+                <div class="card">
+                    <figure>
+                    <img src="./img/inicio/discoduro interno para laptop.jpg" height="200px" class="card-img-top" alt="...">
+                    </figure>
+                    <div class="card-body">
+                        <h5 class="card-title">Disco duro interno para laptop</h5>
+                        <p class="card-text">Disco duro Interno para Laptop 2.5 Seagate 500 GB ST500LT030 interno,2.5″, SATA 3, 5400 rpm y 128MB</p>
+                        <a href="#" class="btn btn-primary">Ver mas</a>
+                    </div>
+                </div>
+            </div>
+            <!-- card 1.2 -->
+            <div class="col-md-4">
+                <div class="card">
+                    <figure>
+                    <img src="./img/inicio/m2 ssd.webp" height="200px" class="card-img-top" alt="...">
+                    </figure>
+                    <div class="card-body">
+                        <h5 class="card-title">SSD M.2 WD Green 2280</h5>
+                        <p class="card-text" style="text-align:justify">La SSD M.2 WD Green 2280 cuenta con almacenamiento mejorado para sus necesidades diarias de computación.</p>
+                        <a href="#" class="btn btn-primary">Ver mas</a>
+                    </div>
+                </div>
+            </div>
+            <!-- card 1.3 -->
+            <div class="col-md-4">
+                <div class="card">
+                    <figure>
+                    <img src="./img/inicio/disipador de calor de aluminio.jpg" height="200px" class="card-img-top" alt="...">
+                    </figure>
+                    <div class="card-body">
+                        <h5 class="card-title">Disipador de calor</h5>
+                        <p class="card-text">Enfriador de CPU de computadora de aluminio de 9 cm Ventilador de refrigeración Disipador de 500 RPM á para LAG 1366 Rojo.
+                            
+</p>
+                        <a href="#" class="btn btn-primary">Ver mas</a>
+                    </div>
+                </div>
+            </div>
     </div>
+    <section class=" mb-3">
+        <img src="./img/scroll/gabinete1.jpg" alt="">
+        <img src="./img/scroll/gabinete2.jpg"alt="">
+        <img src="./img/scroll/gabinete3.jpg"alt="">
+        <img src="./img/scroll/gabinete4.jpg" alt="">
+        <img src="./img/scroll/gabinete5.jpg" alt="">
+        <img src="./img/scroll/gabinete6.jpg" alt="">
+        <img src="./img/scroll/gabinete7.jpg" alt="">
+        <img src="./img/scroll/gabinete8.jpg" alt="">
+    </section>
 
 
-    <footer class="pie-pagina">
-        <?php
-        include(("./componentes/footer.php"))
-        ?>
-    </footer>
+</div>
+<div class="fondo">
+    <div class="slider">
+        <span style="--i:1"><img src="./img/pruebas/1.jpg" alt=""></span>
+        <span style="--i:2"><img src="./img/pruebas/2.jpg" alt=""></span>
+        <span style="--i:3"><img src="./img/pruebas/3.webp" alt=""></span>
+        <span style="--i:4"><img src="./img/pruebas/4.jpg" alt=""></span>
+        <span style="--i:5"><img src="./img/pruebas/5.jpg" alt=""></span>
+        <span style="--i:6"><img src="./img/pruebas/6.jpg" alt=""></span>
+        <span style="--i:7"><img src="./img/pruebas/7.jpg" alt=""></span>
+        <span style="--i:8"><img src="./img/pruebas/8.jpg" alt=""></span>
+    </div>
+    </div>
+<!-- Pie de pagina -->
+        <footer>
+        <?php include("./componentes/footer.php")?>
+        </footer>
 
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
-    <script src="/js/app.js"></script>
+
 </body>
 
 </html>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="/js/app.js"></script>
+    <script>
---
+++ nosotros.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Techno Solutions</title>
+	<link rel="stylesheet" href="../footer-header.css">
+    <link rel="stylesheet" href="../estilo1.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<script src="../script.js"></script>
+</head>
+<body>
+	<header>
+    <?php 
+    include("header.php");
+    ?>
+	</header>
+	<br>
+	<br>
+	<br>
+	<main>
+		<div class="row">
+      
+    <div class="col-sm-6 mb-3 mb-sm-0">
+    <div class="card">
+      
+      <div class="card-body">
+      <span class="badge text-bg-primary"><h2>Techno Solutions    <i class="bi bi-pc-display-horizontal"></i></h2></span>
+        <p class="card-text">La empresa TECNO SOLUCTIONS tiene una empresa de ventas de componentes de computadores como lo son monitores,CPU,tarjetas graficas etc.Pero esta empresa los ultimos meses ha tenido un bajon en usu ventas asi que les propusimos crear una pagina wed para que asi puedan impulsar sus ventas</p>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <br>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+      <span class="badge text-bg-primary"><h2>ALCANCE  <i class="bi bi-globe-americas"></i></i></h2></span>
+        <p class="card-text">Su alcance en la actualidad es local, ya que esta empresa esta localizada en la ciudad de medellin, por lo cual solo tiene ventas en la ciudad ya mencionada, pero con este software queremos que no sea unicamente local si no también nacional y que el producto pueda expandirse y llegar a todo el mundo.</p>      
+      </div>
+    </div>
+    </div>
+    </div>
+<br><br><br>
+    
+		
+  		</div>
+		</div>
+
+		<div class="row">
+  <div class="col-sm-6 mb-3 mb-sm-0">
+    <div class="card">
+      <div class="card-body">
+        <span class="badge text-bg-primary"><h2>MISIÓN  <i class="bi bi-cpu"></i></h2></span>
+        <p class="card-text">La mision principal de este software es ayudar a esta empresa a ser mas reconocida y que sus ventas incrementen. Además, buscar un lugar en el mundo digital por medio de la realización de una pagina web, que facilite la interación Usuario/Cliente con la empresa.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+      <span class="badge text-bg-primary"><h2>VISIÓN   <i class="bi bi-motherboard"></i></h2></span>
+        <p class="card-text"> Visualizar el objetivo a largo plazo en el que la empresa tecnosolutions sea más conocida con su primer software llamado Compu_Start para así posicionarse en el mercado como una de las empresas con más altos  estándares de calidad y eficacia a la hora  desarrollar el software propuesto por el cliente.</p>      </div>
+    </div>
+  </div>
+</div>
+  </section> 
+    
+<br>
+<br>
+<br>
+
+
+<div class="team">
+<span class="badge text-bg-primary"><h2>Equipo de trabajo  <i class="bi bi-people-fill"></i>:</h2></span>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>
+
+	
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>		
+
+	<footer>
+    <?php 
+    include ("footer.php");
+        ?>
+	</footer>
+</body>
+</html>
